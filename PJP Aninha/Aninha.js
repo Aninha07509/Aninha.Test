7a69c3dd7d1b64d8f92b842caf4fb140abd62c59
Aninha07509 <anavitoriacostalinhares@gmail.com>
2026-04-01 20:17:20 -0300

    subir

--- PJP Aninha/Aninha.js
+++ PJP Aninha/Aninha.js
@@ -2,26 +2,5 @@
 for (let i = 10; i >= 0; i--) {
     console.log(i);
   }
-  // Tabuada do 5 
-  for (let i = 1; i <= 10; i++)  {
-    console.log(' 5 x  ${5 * i}');
-  }
-
-
-// Tabuada
-let numero = 5;
-let i = 1;
-while (i <=10)
-{
-    console.log(numero + "x" + i + "= " + numero *i);
-    i++;
-}
-// Contagem regressiva do (while)
-let contador = 5; 
-do {
-    console.log(contador);
-    contador--;
-} 
-while 
-(contador >= 1);
-
+ 
+   
